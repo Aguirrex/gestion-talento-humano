9a9430d8fbb47ce6e13f0e220fc1b93f9ac9c603
Aguirrex <aguirrejuan670@gmail.com>
2024-03-26 19:39:46 -0500

    Configurando la API

---
+++ API/src/test/java/com/ingesoft/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingesoft.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
