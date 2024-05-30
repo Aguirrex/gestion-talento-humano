034ebf9909b0d4d94e4d80c8e85a33ebdcf75425
Juan Jose <juanjose.sierra@utp.edu.co>
2024-05-30 13:16:58 -0500

    Se añadio los endpoints para empleados

---
+++ API/src/main/java/com/ingesoft/api/contrato_persona/ContratoPersonaController.java
@@ -0,0 +1,65 @@
+package com.ingesoft.api.contrato_persona;
+
+import com.ingesoft.api.common.BadRequestsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import static java.lang.Long.parseLong;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class ContratoPersonaController {
+    
+    private final ContratoPersonaService service;
+
+    @PostMapping("/empleado")
+    public ResponseEntity<?> createEmpleado(@RequestBody Map<String, Object> request){
+        try {
+            return ResponseEntity.ok(service.createEmpleado(request));
+        } catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(BadRequestsResponse.builder().message("ERR_EMPLEADO_DATA").build());
+        }
+    }
+
+    @GetMapping("/empleados")
+    public ResponseEntity<?> getEmpleados(){
+        try{
+            return ResponseEntity.ok(service.getEmpleados());
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(BadRequestsResponse.builder().message("ERR_EMPLEADO_DATA"));
+        }
+    }
+
+    @GetMapping("/empleado/{id}")
+    public ResponseEntity<?> getEmpleado(@PathVariable String id){
+        try {
+            return ResponseEntity.ok(service.getEmpleado(parseLong(id)));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(BadRequestsResponse.builder().message("ERR_EMPLEADO_DATA").build());
+        }
+    }
+
+    @GetMapping("/empleado/siguienteId")
+    public ResponseEntity<?> siguienteId(){
+        try {
+            return ResponseEntity.ok(service.siguienteId());
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(BadRequestsResponse.builder().message("ERR_EMPLEADO_DATA").build());
+        }
+    }
+
+    @PutMapping("/empleado/{id}")
+    public ResponseEntity<?> updateEmpleado(
+            @PathVariable String id,
+            @RequestBody Map<String, Object> request
+    ){
+        try {
+            return ResponseEntity.ok(service.updateEmpleado(request, parseLong(id)));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(BadRequestsResponse.builder().message("ERR_EMPLEADO_DATA").build());
+        }
+    }
+}
